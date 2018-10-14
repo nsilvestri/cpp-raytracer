cbb62219bd2833fcefb5e4e293995431f2b80bf5
Nick Silvestri <nickpsilvestri@gmail.com>
2018-10-13 19:53:56 -0700

    changed Sphere miss testcase

--- test/tests-Sphere.cpp
+++ test/tests-Sphere.cpp
@@ -13,13 +13,15 @@ TEST_CASE("Test intersection", "[Sphere]")
     Ray3D alongX = Ray3D(Vector3D(-100, 0, 0), Vector3D(1, 0, 0));
     REQUIRE(s.intersect(ir, alongX));
     REQUIRE(ir.normalAtIntersection == Vector3D(-1, 0, 0));
+    REQUIRE(ir.normalAtIntersection != Vector3D(-1, 5, 0));
     REQUIRE(ir.pointOfIntersection == Vector3D(-1, 0, 0));
+    REQUIRE(ir.pointOfIntersection != Vector3D(-1, 5, 0));
 
 
     Ray3D alongY = Ray3D(Vector3D(0, 100, 0), Vector3D(0, 1, 0));
     Ray3D alongZ = Ray3D(Vector3D(0, 0, 100), Vector3D(0, 0, 1));
     Ray3D graze = Ray3D(Vector3D(-1, 1, 0), Vector3D(1, 0, 0));
-    Ray3D miss = Ray3D(Vector3D(-1, 1, 0), Vector3D(0, 1, 0));
+    Ray3D miss = Ray3D(Vector3D(-10, 10, 0), Vector3D(0, 1, 0));
 
     REQUIRE(s.intersect(ir, alongY));
     REQUIRE(s.intersect(ir, alongZ));
