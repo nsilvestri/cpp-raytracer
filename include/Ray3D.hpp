97e951b7d594deced858d1771413c2af94c24937
Nick Silvestri <nickpsilvestri@gmail.com>
2019-03-12 19:12:52 -0700

    moved Ray3D documenation to header file

--- include/Ray3D.hpp
+++ include/Ray3D.hpp
@@ -5,22 +5,64 @@
 class Ray3D
 {
     private:
+
         Vector3D origin;
         Vector3D direction;
+
     public:
-        Ray3D();
+
+        /**
+         * Constructor for Ray3D. Creates a new Ray3D object based on the given origin
+         * and direction, given as Vector3D objects.
+         * @param origin the origin of this new Ray3D
+         * @param direction the direction of this new Ray3D. The direction will be
+         * normalized.
+         */
         Ray3D(Vector3D origin, Vector3D direction);
+
+        /**
+         * Default constructor of Ray3D. Sets the origin and direction to Vector3Ds with
+         * initial values of 0.
+         */
+        Ray3D();
+
+        /**
+         * Copy constructor for Ray3D
+         */
         Ray3D(const Ray3D& r);
 
+        /**
+         * Gets the origin of this Ray3D as a Vector3D.
+         * @returns the origin of this Ray3D as a Vector3D.
+         */
         Vector3D getOrigin() const;
+
+        /**
+         * Gets the direction of this Ray3D as a Vector3D.
+         * @returns the direction of this Ray3D as a Vector3D.
+         */
         Vector3D getDirection() const;
 
+        /**
+         * Sets the origin of this Ray3D to the given origin.
+         * @param origin the new origin to set this Ray3D to.
+         */
         void setOrigin(Vector3D origin);
+
+        /**
+         * Sets the direction of this Ray3D to the given direction. The direction will
+         * be normalized.
+         * @param direction the new direction to set this Ray3D to.
+         */
         void setDirection(Vector3D direction);
 
+        /**
+         * Determines if the given Vector3D, interpreted as a point, lies on this Ray3D.
+         * @param point the Vector3D point to check if it lies on this Ray3D.
+         */
         bool isPointOn(Vector3D point) const;
 
+        
         friend std::ostream& operator<<(std::ostream &stream, const Ray3D& r);
-
 };
 
--- src/Ray3D.cpp
+++ src/Ray3D.cpp
@@ -4,79 +4,45 @@
 #include "Ray3D.hpp"
 #include "Vector3D.hpp"
 
-/**
- * Constructor for Ray3D. Creates a new Ray3D object based on the given origin
- * and direction, given as Vector3D objects.
- * @param origin the origin of this new Ray3D
- * @param direction the direction of this new Ray3D. The direction will be
- * normalized.
- */
+
 Ray3D::Ray3D(Vector3D origin, Vector3D direction)
 {
     this->setOrigin(origin);
     this->setDirection(direction.normalize());
 }
 
-/**
- * Default constructor of Ray3D. Sets the origin and direction to Vector3Ds with
- * initial values of 0.
- */
 Ray3D::Ray3D()
 {
     this->setOrigin(Vector3D(0, 0, 0));
     this->setDirection(Vector3D(0, 0, 0));
 }
 
-/**
- * Copy constructor for Ray3D
- */
 Ray3D::Ray3D(const Ray3D& r)
 {
     this->setOrigin(r.getOrigin());
     this->setDirection(r.getDirection().normalize());
 }
 
-/**
- * Gets the origin of this Ray3D as a Vector3D.
- * @returns the origin of this Ray3D as a Vector3D.
- */
 Vector3D Ray3D::getOrigin() const
 {
     return this->origin;
 }
 
-/**
- * Gets the direction of this Ray3D as a Vector3D.
- * @returns the direction of this Ray3D as a Vector3D.
- */
 Vector3D Ray3D::getDirection() const
 {
     return this->direction;
 }
 
-/**
- * Sets the origin of this Ray3D to the given origin.
- * @param origin the new origin to set this Ray3D to.
- */
 void Ray3D::setOrigin(Vector3D origin)
 {
     this->origin = origin;
 }
 
-/**
- * Sets the direction of this Ray3D to the given direction. The direction will
- * be normalized.
- * @param direction the new direction to set this Ray3D to.
- */
 void Ray3D::setDirection(Vector3D direction)
 {
     this->direction = direction.normalize();
 }
 
-/**
- * Determines if the given Vector3D, interpreted as a point, lies on this Ray3D.
- * @param point the Vector3D point to check if it lies on this Ray3D.
- */
 bool Ray3D::isPointOn(Vector3D point) const
 {
     Vector3D pointDirection = point - this->getOrigin();
